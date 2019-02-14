0e0be80eb81a9ac256a76b67e26937a519f6563e
Shinji Koide <koide.shinji@gmail.com>
2019-02-14 22:55:38 +0900

    MyApp2 modify

--- MyApp2.kt
+++ MyApp2.kt
@@ -5,14 +5,18 @@ fun main(vararg args: String){
 	val red: Color;
 	red = Color.RED;
 	
+	val green: Color;
+	green = Color.GREEN;
+	
 	println(Integer.toString(red.r));
+	println(Integer.toString(green.g));
 }
 
-enum class Color constructor(){
+enum class Color constructor(val r: Int, val g: Int, val b: Int){
 	RED(255, 0, 0), 
 	GREEN(0, 255, 0), 
 	BLUE(0, 0, 255);
-	
+	/*
 	var r: Int;
 	var g: Int;
 	var b: Int;
@@ -22,11 +26,13 @@ enum class Color constructor(){
 		this.g = 99;
 		this.b = 99;
 	}
-	
+	*/
+	/*
 	// secondary constructor
 	constructor(r: Int, g: Int, b: Int): this(){
 		this.r = r;
 		this.g = g;
 		this.b = b;
 	}
+	*/
 }
